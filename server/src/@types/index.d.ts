05c22a64939e3d43c94f8aeecb24e74bca87cba1
kokily <kokily@naver.com>
2021-06-20 14:52:35 +0900

    GraphQL API reserve 작성

--- server/src/@types/index.d.ts
+++ server/src/@types/index.d.ts
@@ -1,11 +1,24 @@
-export const typeDefs = ["type CheckMeResponse {\n  ok: Boolean!\n  error: String\n  me: MeType\n}\n\ntype Query {\n  CheckMe: CheckMeResponse!\n  ViewCart: ViewCartResponse!\n  ListItems(cursor: ID, name: String, divide: String, native: String): ListItemsResponse!\n  ReadItem(id: ID!): ReadItemResponse!\n}\n\ntype LoginResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  Login(username: String!, password: String!): LoginResponse!\n  Password(password: String!): PasswordResponse!\n  Register(username: String!, password: String!): RegisterResponse!\n  AddCart(item_id: ID!, count: Int!, price: Int!): AddCartResponse!\n  RemoveCart: RemoveCartResponse!\n  RemoveCartItem(item_id: ID!): RemoveCartItemResponse!\n  AddItem(name: String!, divide: String!, native: String!, unit: String!, price: Int!): AddItemResponse!\n  RemoveItem(id: ID!): RemoveItemResponse!\n  UpdateItem(id: ID!, name: String, divide: String, native: String, unit: String, price: Int): UpdateItemResponse!\n}\n\ntype PasswordResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype RegisterResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype AddCartResponse {\n  ok: Boolean!\n  error: String\n  cart: Cart\n}\n\ntype RemoveCartResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype RemoveCartItemResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype ViewCartResponse {\n  ok: Boolean!\n  error: String\n  cart: Cart\n}\n\ntype AddItemResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype ListItemsResponse {\n  ok: Boolean!\n  error: String\n  items: [Item]\n}\n\ntype ReadItemResponse {\n  ok: Boolean!\n  error: String\n  item: Item\n}\n\ntype RemoveItemResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype UpdateItemResponse {\n  ok: Boolean!\n  error: String\n}\n\nscalar Date\n\ntype User {\n  id: ID!\n  username: String!\n  password: String!\n  admin: Boolean!\n  created_at: Date!\n  updated_at: Date\n}\n\ntype MeType {\n  user_id: ID!\n  username: String!\n  admin: Boolean!\n}\n\ntype InputItem {\n  id: ID!\n  name: String!\n  divide: String!\n  native: String!\n  unit: String!\n  price: Int!\n  count: Int!\n  amount: Int!\n}\n\ntype Item {\n  id: ID!\n  num: Int!\n  name: String!\n  divide: String!\n  native: String!\n  unit: String!\n  price: Int!\n  created_at: Date!\n  updated_at: Date\n}\n\ntype Cart {\n  id: ID!\n  items: [InputItem]!\n  completed: Boolean!\n  deleted: Boolean!\n  created_at: Date!\n  updated_at: Date\n  user_id: String!\n  bill_id: String\n}\n\ntype Bill {\n  id: ID!\n  title: String!\n  hall: String!\n  etc: String!\n  total_amount: Int!\n  items: [InputItem]\n  reserve: Int\n  username: String!\n  user_id: String!\n  cart_id: String!\n  created_at: Date!\n}\n\ntype Wedding {\n  id: ID!\n  husband_name: String!\n  husband_image: String\n  bride_name: String!\n  bride_image: String\n  wedding_at: String!\n  event_at: String!\n  cost_husband: Int!\n  cost_bride: Int!\n  meal_husband: Int!\n  meal_bride: Int!\n  present_husband: Int!\n  present_bride: Int!\n  reserve_husband: Int!\n  reserve_bride: Int!\n  created_at: Date!\n  updated_at: Date\n  conventionId: String\n  convention: Convention\n  companyId: String\n  company: Company\n  hanbokId: String\n  hanbok: Hanbok\n  eventId: String\n  event: Event\n  mealId: String\n  meal: Meal\n  presentId: String\n  present: Present\n  reserveId: String\n  reserve: Reserve\n}\n\ntype Company {\n  id: ID!\n  company_husband: Int!\n  company_bride: Int!\n  rooftop_husband: Int!\n  rooftop_bride: Int!\n  owner_woman_husband: Int!\n  owner_woman_bride: Int!\n  owner_man_husband: Int!\n  owner_man_bride: Int!\n  select_husband: Int!\n  select_bride: Int!\n  frame_husband: Int!\n  frame_bride: Int!\n  dress_husband: Int!\n  dress_bride: Int!\n  hairpin_husband: Int!\n  hairpin_bride: Int!\n  wig_husband: Int!\n  wig_bride: Int!\n  video_husband: Int!\n  video_bride: Int!\n  etc_husband: Int!\n  etc_bride: Int!\n  weddingId: String!\n  wedding: Wedding!\n}\n\ntype Convention {\n  id: ID!\n  rental_husband: Int!\n  rental_bride: Int!\n  sword_husband: Int!\n  sword_bride: Int!\n  glove_husband: Int!\n  glove_bride: Int!\n  bouquet_husband: Int!\n  bouquet_bride: Int!\n  ceremony_husband: Int!\n  ceremony_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Event {\n  id: ID!\n  play_husband: Int!\n  play_bride: Int!\n  anthem_husband: Int!\n  anthem_bride: Int!\n  moderator_husband: Int!\n  moderator_bride: Int!\n  officiate_husband: Int!\n  officiate_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Hanbok {\n  id: ID!\n  hanbok_pre_husband: Int!\n  hanbok_pre_bride: Int!\n  hanbok_post_husband: Int!\n  hanbok_post_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Meal {\n  id: ID!\n  meals: String!\n  meals_price: Int!\n  meals_num_husband: Int!\n  meals_num_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Present {\n  id: ID!\n  present: String!\n  present_price: Int!\n  present_num_husband: Int!\n  present_num_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Reserve {\n  id: ID!\n  reserve: String!\n  reserve_pay: Int!\n  weddingId: String\n  wedding: Wedding\n}\n"];
+export const typeDefs = ["type CheckMeResponse {\n  ok: Boolean!\n  error: String\n  me: MeType\n}\n\ntype Query {\n  CheckMe: CheckMeResponse!\n  ListBills(cursor: ID, user_id: ID, title: String, hall: String): ListBillsResponse!\n  ReadBill(id: ID!): ReadBillResponse!\n  ViewCart: ViewCartResponse!\n  ListItems(cursor: ID, name: String, divide: String, native: String): ListItemsResponse!\n  ReadItem(id: ID!): ReadItemResponse!\n}\n\ntype LoginResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  Login(username: String!, password: String!): LoginResponse!\n  Password(password: String!): PasswordResponse!\n  Register(username: String!, password: String!): RegisterResponse!\n  AddBill(title: String!, hall: String!, etc: String!): AddBillResponse!\n  AddReserve(bill_id: ID!, reserve: Int!): AddReserveResponse!\n  RemoveBill(id: ID!): RemoveBillResponse!\n  RemoveReserve(bill_id: ID!): RemoveReserveResponse!\n  RestoreBill(id: ID!): RestoreBillResponse!\n  AddCart(item_id: ID!, count: Int!, price: Int!): AddCartResponse!\n  RemoveCart: RemoveCartResponse!\n  RemoveCartItem(item_id: ID!): RemoveCartItemResponse!\n  AddItem(name: String!, divide: String!, native: String!, unit: String!, price: Int!): AddItemResponse!\n  RemoveItem(id: ID!): RemoveItemResponse!\n  UpdateItem(id: ID!, name: String, divide: String, native: String, unit: String, price: Int): UpdateItemResponse!\n}\n\ntype PasswordResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype RegisterResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype AddBillResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype AddReserveResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype ListBillsResponse {\n  ok: Boolean!\n  error: String\n  bills: [Bill]\n}\n\ntype ReadBillResponse {\n  ok: Boolean!\n  error: String\n  bill: Bill\n}\n\ntype RemoveBillResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype RemoveReserveResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype RestoreBillResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype AddCartResponse {\n  ok: Boolean!\n  error: String\n  cart: Cart\n}\n\ntype RemoveCartResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype RemoveCartItemResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype ViewCartResponse {\n  ok: Boolean!\n  error: String\n  cart: Cart\n}\n\ntype AddItemResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype ListItemsResponse {\n  ok: Boolean!\n  error: String\n  items: [Item]\n}\n\ntype ReadItemResponse {\n  ok: Boolean!\n  error: String\n  item: Item\n}\n\ntype RemoveItemResponse {\n  ok: Boolean!\n  error: String\n}\n\ntype UpdateItemResponse {\n  ok: Boolean!\n  error: String\n}\n\nscalar Date\n\ntype User {\n  id: ID!\n  username: String!\n  password: String!\n  admin: Boolean!\n  created_at: Date!\n  updated_at: Date\n}\n\ntype MeType {\n  user_id: ID!\n  username: String!\n  admin: Boolean!\n}\n\ntype InputItem {\n  id: ID!\n  name: String!\n  divide: String!\n  native: String!\n  unit: String!\n  price: Int!\n  count: Int!\n  amount: Int!\n}\n\ntype Item {\n  id: ID!\n  num: Int!\n  name: String!\n  divide: String!\n  native: String!\n  unit: String!\n  price: Int!\n  created_at: Date!\n  updated_at: Date\n}\n\ntype Cart {\n  id: ID!\n  items: [InputItem]!\n  completed: Boolean!\n  deleted: Boolean!\n  created_at: Date!\n  updated_at: Date\n  user_id: String!\n  bill_id: String\n}\n\ntype Bill {\n  id: ID!\n  title: String!\n  hall: String!\n  etc: String!\n  total_amount: Int!\n  items: [InputItem]\n  reserve: Int\n  username: String!\n  user_id: String!\n  cart_id: String!\n  created_at: Date!\n}\n\ntype Wedding {\n  id: ID!\n  husband_name: String!\n  husband_image: String\n  bride_name: String!\n  bride_image: String\n  wedding_at: String!\n  event_at: String!\n  cost_husband: Int!\n  cost_bride: Int!\n  meal_husband: Int!\n  meal_bride: Int!\n  present_husband: Int!\n  present_bride: Int!\n  reserve_husband: Int!\n  reserve_bride: Int!\n  created_at: Date!\n  updated_at: Date\n  conventionId: String\n  convention: Convention\n  companyId: String\n  company: Company\n  hanbokId: String\n  hanbok: Hanbok\n  eventId: String\n  event: Event\n  mealId: String\n  meal: Meal\n  presentId: String\n  present: Present\n  reserveId: String\n  reserve: Reserve\n}\n\ntype Company {\n  id: ID!\n  company_husband: Int!\n  company_bride: Int!\n  rooftop_husband: Int!\n  rooftop_bride: Int!\n  owner_woman_husband: Int!\n  owner_woman_bride: Int!\n  owner_man_husband: Int!\n  owner_man_bride: Int!\n  select_husband: Int!\n  select_bride: Int!\n  frame_husband: Int!\n  frame_bride: Int!\n  dress_husband: Int!\n  dress_bride: Int!\n  hairpin_husband: Int!\n  hairpin_bride: Int!\n  wig_husband: Int!\n  wig_bride: Int!\n  video_husband: Int!\n  video_bride: Int!\n  etc_husband: Int!\n  etc_bride: Int!\n  weddingId: String!\n  wedding: Wedding!\n}\n\ntype Convention {\n  id: ID!\n  rental_husband: Int!\n  rental_bride: Int!\n  sword_husband: Int!\n  sword_bride: Int!\n  glove_husband: Int!\n  glove_bride: Int!\n  bouquet_husband: Int!\n  bouquet_bride: Int!\n  ceremony_husband: Int!\n  ceremony_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Event {\n  id: ID!\n  play_husband: Int!\n  play_bride: Int!\n  anthem_husband: Int!\n  anthem_bride: Int!\n  moderator_husband: Int!\n  moderator_bride: Int!\n  officiate_husband: Int!\n  officiate_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Hanbok {\n  id: ID!\n  hanbok_pre_husband: Int!\n  hanbok_pre_bride: Int!\n  hanbok_post_husband: Int!\n  hanbok_post_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Meal {\n  id: ID!\n  meals: String!\n  meals_price: Int!\n  meals_num_husband: Int!\n  meals_num_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Present {\n  id: ID!\n  present: String!\n  present_price: Int!\n  present_num_husband: Int!\n  present_num_bride: Int!\n  weddingId: String\n  wedding: Wedding\n}\n\ntype Reserve {\n  id: ID!\n  reserve: String!\n  reserve_pay: Int!\n  weddingId: String\n  wedding: Wedding\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   CheckMe: CheckMeResponse;
+  ListBills: ListBillsResponse;
+  ReadBill: ReadBillResponse;
   ViewCart: ViewCartResponse;
   ListItems: ListItemsResponse;
   ReadItem: ReadItemResponse;
+}
+
+export interface ListBillsQueryArgs {
+  cursor: string | null;
+  user_id: string | null;
+  title: string | null;
+  hall: string | null;
+}
+
+export interface ReadBillQueryArgs {
+  id: string;
 }
 
 export interface ListItemsQueryArgs {
@@ -31,6 +44,45 @@ export interface MeType {
   admin: boolean;
 }
 
+export interface ListBillsResponse {
+  ok: boolean;
+  error: string | null;
+  bills: Array<Bill> | null;
+}
+
+export interface Bill {
+  id: string;
+  title: string;
+  hall: string;
+  etc: string;
+  total_amount: number;
+  items: Array<InputItem> | null;
+  reserve: number | null;
+  username: string;
+  user_id: string;
+  cart_id: string;
+  created_at: Date;
+}
+
+export interface InputItem {
+  id: string;
+  name: string;
+  divide: string;
+  native: string;
+  unit: string;
+  price: number;
+  count: number;
+  amount: number;
+}
+
+export type Date = any;
+
+export interface ReadBillResponse {
+  ok: boolean;
+  error: string | null;
+  bill: Bill | null;
+}
+
 export interface ViewCartResponse {
   ok: boolean;
   error: string | null;
@@ -47,19 +99,6 @@ export interface Cart {
   user_id: string;
   bill_id: string | null;
 }
-
-export interface InputItem {
-  id: string;
-  name: string;
-  divide: string;
-  native: string;
-  unit: string;
-  price: number;
-  count: number;
-  amount: number;
-}
-
-export type Date = any;
 
 export interface ListItemsResponse {
   ok: boolean;
@@ -89,6 +128,11 @@ export interface Mutation {
   Login: LoginResponse;
   Password: PasswordResponse;
   Register: RegisterResponse;
+  AddBill: AddBillResponse;
+  AddReserve: AddReserveResponse;
+  RemoveBill: RemoveBillResponse;
+  RemoveReserve: RemoveReserveResponse;
+  RestoreBill: RestoreBillResponse;
   AddCart: AddCartResponse;
   RemoveCart: RemoveCartResponse;
   RemoveCartItem: RemoveCartItemResponse;
@@ -109,6 +153,29 @@ export interface PasswordMutationArgs {
 export interface RegisterMutationArgs {
   username: string;
   password: string;
+}
+
+export interface AddBillMutationArgs {
+  title: string;
+  hall: string;
+  etc: string;
+}
+
+export interface AddReserveMutationArgs {
+  bill_id: string;
+  reserve: number;
+}
+
+export interface RemoveBillMutationArgs {
+  id: string;
+}
+
+export interface RemoveReserveMutationArgs {
+  bill_id: string;
+}
+
+export interface RestoreBillMutationArgs {
+  id: string;
 }
 
 export interface AddCartMutationArgs {
@@ -158,6 +225,31 @@ export interface RegisterResponse {
   error: string | null;
 }
 
+export interface AddBillResponse {
+  ok: boolean;
+  error: string | null;
+}
+
+export interface AddReserveResponse {
+  ok: boolean;
+  error: string | null;
+}
+
+export interface RemoveBillResponse {
+  ok: boolean;
+  error: string | null;
+}
+
+export interface RemoveReserveResponse {
+  ok: boolean;
+  error: string | null;
+}
+
+export interface RestoreBillResponse {
+  ok: boolean;
+  error: string | null;
+}
+
 export interface AddCartResponse {
   ok: boolean;
   error: string | null;
@@ -196,20 +288,6 @@ export interface User {
   admin: boolean;
   created_at: Date;
   updated_at: Date | null;
-}
-
-export interface Bill {
-  id: string;
-  title: string;
-  hall: string;
-  etc: string;
-  total_amount: number;
-  items: Array<InputItem> | null;
-  reserve: number | null;
-  username: string;
-  user_id: string;
-  cart_id: string;
-  created_at: Date;
 }
 
 export interface Wedding {
