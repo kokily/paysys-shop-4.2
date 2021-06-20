8cb365f2ecacdc1884645a8c0384348566771895
kokily <kokily@naver.com>
2021-06-20 15:17:08 +0900

    백엔드 서버 SSL 작업 (Certbot)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,7 @@
 import 'dotenv/config';
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 import { createConnection } from 'typeorm';
 import app from './app';
 import ConnectionOptions from './libs/ormConfig';
@@ -6,9 +9,38 @@ import ConnectionOptions from './libs/ormConfig';
 const _bootStrap = async () => {
   try {
     await createConnection(ConnectionOptions);
-    await app.listen(4000, () => {
-      console.log('http://localhost:4000/graphql');
-    });
+
+    const configurations = {
+      production: { ssl: true, port: 443, hostname: 'paysys.shop' },
+      development: { ssl: false, port: 4000, hostname: 'localhost' },
+    };
+    const environment = process.env.NODE_ENV || 'production';
+    const config = configurations[environment];
+
+    let httpServer;
+    let httpsServer;
+
+    if (config.ssl) {
+      httpServer = http.createServer(app.callback());
+      httpsServer = https.createServer(
+        {
+          key: fs.readFileSync(`${process.env.SSL_KEY}`),
+          cert: fs.readFileSync(`${process.env.SSL_CERT}`),
+        },
+        app.callback()
+      );
+
+      httpServer.listen(80);
+      httpsServer.listen(config.port, () => {
+        console.log(`> Paysys server on https://${config.hostname}:${config.port}`);
+      });
+    } else {
+      httpServer = http.createServer(app.callback);
+
+      httpServer.listen(config.port, () => {
+        console.log(`> Paysys dev server on http://${config.hostname}:${config.port}`);
+      });
+    }
   } catch (err) {
     console.error(err);
   }
